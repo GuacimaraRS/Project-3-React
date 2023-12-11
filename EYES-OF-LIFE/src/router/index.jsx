15d0b1201ff2dced5e30f758aeb351e26959aa23
dani <danitogc24@gmail.com>
2023-12-11 17:44:47 +0000

    pages arregladas

--- EYES-OF-LIFE/src/router/index.jsx
+++ EYES-OF-LIFE/src/router/index.jsx
@@ -2,15 +2,15 @@ import { createBrowserRouter } from "react-router-dom";
 import Home from "../pages/Home/Home";
 import Root from "../Layouts/LayoutHome/Layouts";
 import AboutUs from "../pages/AboutUs/AboutUs";
-import SignIn from "../components/SingIn/SignIn";
-import SignUp from "../components/SignUp/SignUp";
-
+import SignIn from "../pages/SingIn/SignIn";
+import SignUp from "../pages/SignUp/SignUp";
 import LayoutPerfil from "../Layouts/LayoutPerfil/LayoutPerfil";
 import MyMessages from "../pages/MyMessages/MyMessages";
 import ProfilePhotographer from "../pages/ProfilePhotographer/ProfilePhotographer";
 import MyDates from "../pages/MyDates/MyDates";
 import MyGalery from "../pages/MyGalery/MyGalery";
 import MyEvents from "../pages/MyEvents/MyEvents";
+
 const router = createBrowserRouter([
   {
     path: "/",
@@ -20,7 +20,7 @@ const router = createBrowserRouter([
         path: "/",
         element: <Home />,
       },
-   
+
       {
         path: "/about-us",
         element: <AboutUs />,
@@ -33,16 +33,13 @@ const router = createBrowserRouter([
       {
         path: "/sign-in",
         element: <SignIn />,
-      }
+      },
     ],
   },
-  
 
-
-  
   {
     path: "/",
-    element: <LayoutPerfil/>,
+    element: <LayoutPerfil />,
     children: [
       {
         path: "/profile-photographer",
@@ -64,13 +61,8 @@ const router = createBrowserRouter([
         path: "/my-events",
         element: <MyEvents />,
       },
-    ], 
-    
-    
-
-    
+    ],
   },
- 
 ]);
 
 export default router;
